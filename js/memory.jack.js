941b20b0d613f0a6bf687caa40a4f6f31ee011e2
andberg <andreaberglund@hotmail.com>
2013-10-21 19:59:55 +0200

    Changes so you see the Jack-card before the array starts and everything is work and no play..

--- js/memory.jack.js
+++ js/memory.jack.js
@@ -6,18 +6,19 @@ var i = 0,
     playSound,
     canIclick,
     answer,
+    stopJack = 0,
     jack, jackArray = ["A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y. ", "A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y. ", "A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y. ", "A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y. ", "A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y. ", "A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y. ", "A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y. ", "A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y. ", "A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y.", "A", "l", "l ", "w", "o", "r", "k ", "a", "n", "d ", "n", "o ", "p", "l", "a", "y ", "m", "a", "k", "e", "s ", "J", "a", "c", "k ", "a ", "d", "u", "l", "l ", "b", "o", "y.", " "],
-    stopJack = 0;
-memory.jack = function () {
+    runJack;
+
+runJack = function () {
     "use strict";
     if (stopJack < 220) {
-        $("h1").text("GameOver");
         $("#points").hide();
         $(".frame").hide();
         setTimeout(function () {
             $("h2").text($("h2").text() + jackArray[i]);
             i = i + 1;
-            memory.jack();
+            runJack();
         }, 40);
         stopJack = stopJack + 1;
     } else {
@@ -25,19 +26,23 @@ memory.jack = function () {
         if (answer === ("yes" || "Yes")) {
             location.reload();
         } else {
-            $("h2").hide();
-            $("#points").show();
-            $(".frame").show();
-            $(".bricks").css("background-image", "url(images/jack.jpg)");
-            audio = $("audio")[0];
-            playSound = function () {
-                audio.load();
-                audio.play();
-            };
-            playSound();
-            setTimeout(function () {
-                window.location = "http://24.media.tumblr.com/f4a1ae4af719fde1a3c09ddbd1418eb6/tumblr_mnl942xI9r1snnccgo1_500.gif";
-            }, 4000);
+            window.location = "http://24.media.tumblr.com/f4a1ae4af719fde1a3c09ddbd1418eb6/tumblr_mnl942xI9r1snnccgo1_500.gif";
         }
     }
+};
+
+memory.jack = function () {
+    "use strict";
+    $("h1").text("GameOver");
+    $("#points").hide();
+    $(".bricks").css("background-image", "url(images/jack.jpg)");
+    audio = $("audio")[0];
+    playSound = function () {
+        audio.load();
+        audio.play();
+    };
+    playSound();
+    setTimeout(function () {
+        runJack();
+    }, 3000);
 };
