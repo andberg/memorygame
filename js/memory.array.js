fe7814a3f347acb10c0d18715a14f96da073f569
Andrea Berglund <andreaberglund@outlook.com>
2020-03-04 12:21:58 +0100

    Small adjustments

--- js/memory.array.js
+++ js/memory.array.js
@@ -1,22 +1,32 @@
 /*jslint browser:true */
 /*global $: false, memory: false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
 
-memory.array = _.shuffle([{
-    url: "images/1.jpg"
-}, {
-    url: "images/2.jpg"
-}, {
-    url: "images/3.jpg"
-}, {
-    url: "images/4.jpg"
-}, {
-    url: "images/1.jpg"
-}, {
-    url: "images/2.jpg"
-}, {
-    url: "images/3.jpg"
-}, {
-    url: "images/4.jpg"
-}, {
-    url: "images/jack.jpg"
-}]);
+memoryGame.array = _.shuffle([
+  {
+    url: 'images/1.jpg',
+  },
+  {
+    url: 'images/2.jpg',
+  },
+  {
+    url: 'images/3.jpg',
+  },
+  {
+    url: 'images/4.jpg',
+  },
+  {
+    url: 'images/1.jpg',
+  },
+  {
+    url: 'images/2.jpg',
+  },
+  {
+    url: 'images/3.jpg',
+  },
+  {
+    url: 'images/4.jpg',
+  },
+  {
+    url: 'images/jack.jpg',
+  },
+]);
--- js/memory.play.js
+++ js/memory.play.js
@@ -1,58 +1,60 @@
 /*jslint browser:true */
 /*global $: false, memory: false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
 
-$(function () {
-    "use strict";
-    var card1,
-        div2,
-        card2,
-        div1,
-        countClick = 0,
-        points = 0,
-        canIclick = true,
-        hideWrongAnswer = function (div1, div2) {
-            setTimeout(function () {
-                div1.css("background-image", "url(images/monster.jpg)");
-                div2.css("background-image", "url(images/monster.jpg)");
-                canIclick = true;
-            }, 1500);
-        };
+$(function() {
+  'use strict';
+  var card1,
+    div2,
+    card2,
+    div1,
+    countClick = 0,
+    points = 0,
+    canIclick = true,
+    hideWrongAnswer = function(div1, div2) {
+      setTimeout(function() {
+        div1.css('background-image', 'url(images/monster.jpg)');
+        div2.css('background-image', 'url(images/monster.jpg)');
+        canIclick = true;
+      }, 1500);
+    };
 
-    $(".result p").text("Points: " + points);
-    $(".frame div").click(function () {
-        if (canIclick) {
-            var i = Number($(this).attr("id").replace("div", ""));
-            $(this).css("background-image", "url(" + memory.array[i].url + ")");
-            if (countClick === 0) {
-                countClick = countClick + 1;
-                div1 = $(this);
-                card1 = memory.array[i].url;
-                if (card1 === "images/jack.jpg") {
-                    memory.jack();
-                }
-            } else {
-                div2 = $(this);
-                if (div2.attr("id") !== div1.attr("id")) {
-                    card2 = memory.array[i].url;
-                    if (card2 === "images/jack.jpg") {
-                        memory.jack();
-                    } else {
-
-                        if (card1 !== card2) {
-                            canIclick = false;
-                            hideWrongAnswer(div1, div2);
-
-                        } else {
-                            points = points + 1;
-                            if (points === 4) {
-                                memory.win();
-                            }
-                            $(".result p").text("Points " + points);
-                        }
-                        countClick = 0;
-                    }
-                }
-            }
+  $('.result p').text('Points: ' + points);
+  $('.frame div').click(function() {
+    if (canIclick) {
+      var i = Number(
+        $(this)
+          .attr('id')
+          .replace('div', ''),
+      );
+      $(this).css('background-image', 'url(' + memoryGame.array[i].url + ')');
+      if (countClick === 0) {
+        countClick = countClick + 1;
+        div1 = $(this);
+        card1 = memoryGame.array[i].url;
+        if (card1 === 'images/jack.jpg') {
+          memoryGame.jack();
         }
-    });
+      } else {
+        div2 = $(this);
+        if (div2.attr('id') !== div1.attr('id')) {
+          card2 = memoryGame.array[i].url;
+          if (card2 === 'images/jack.jpg') {
+            memoryGame.jack();
+          } else {
+            if (card1 !== card2) {
+              canIclick = false;
+              hideWrongAnswer(div1, div2);
+            } else {
+              points = points + 1;
+              if (points === 4) {
+                memoryGame.win();
+              }
+              $('.result p').text('Points ' + points);
+            }
+            countClick = 0;
+          }
+        }
+      }
+    }
+  });
 });
